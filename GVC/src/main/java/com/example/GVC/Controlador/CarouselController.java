859b95c4bbe1051f543eef7c00650db21ea39ca8
Erick Osuna Garcia <127696393+Erickzzxc@users.noreply.github.com>
2024-10-30 22:47:48 -0700

    PBI-EVN-US11 Crear la interfaz de home en templeates

---
+++ GVC/src/main/java/com/example/GVC/Controlador/CarouselController.java
@@ -0,0 +1,45 @@
+package com.example.GVC.Controlador;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class CarouselController {
+
+  private final OidcUserService oidcUser;
+
+  public CarouselController(OidcUserService oidcUser) {
+    this.oidcUser = oidcUser;
+  }
+
+  @GetMapping("/home")
+  public String galeria(@AuthenticationPrincipal OidcUser oidcUser, Model model) {
+    List<String> imagenes = Arrays.asList(
+            "/Evento1.jpg",
+            "/ojo2.jpg",
+            "/ojo3.jpg"
+    );
+
+    String nombre = "Invitado"; // Valor por defecto
+    String email = "No disponible"; // Valor por defecto
+
+    if (oidcUser != null) {
+      nombre = (String) oidcUser.getAttribute("name");
+      email = (String) oidcUser.getAttribute("email");
+    }
+    // Puedes reemplazar estos valores con los datos del usuario actual
+    model.addAttribute("nombre", nombre);
+    model.addAttribute("email", email);
+
+    model.addAttribute("imagenes", imagenes);
+    return "home";
+  }
+
+  }
